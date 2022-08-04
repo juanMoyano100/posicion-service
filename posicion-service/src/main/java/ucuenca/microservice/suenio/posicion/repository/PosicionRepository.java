167ea5a0f0af20f8cceb4aaac43e5139c627c995
Juan Moyano <juan.cranex@hotmail.com>
2022-08-04 00:00:53 -0500

    config

--- posicion-service/src/main/java/ucuenca/microservice/suenio/posicion/repository/PosicionRepository.java
+++
@@ -1,8 +0,0 @@
-package ucuenca.microservice.suenio.posicion.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import ucuenca.microservice.suenio.posicion.entity.PosicionEntity;
-
-public interface PosicionRepository extends MongoRepository <PosicionEntity, String> {
-
-}
