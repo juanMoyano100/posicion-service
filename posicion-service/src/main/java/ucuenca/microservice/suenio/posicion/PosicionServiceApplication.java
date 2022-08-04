9ccafb39b3bd8146731dedb27c3123fdd0b7122f
juankll2 <juanc.lazol@ucuenca.edu.ec>
2022-08-03 23:18:37 -0500

    subida del servicio de posicion

---
+++ posicion-service/src/main/java/ucuenca/microservice/suenio/posicion/PosicionServiceApplication.java
@@ -0,0 +1,15 @@
+package ucuenca.microservice.suenio.posicion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PosicionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosicionServiceApplication.class, args);
+	}
+
+}
---
+++ posicion-service/src/test/java/ucuenca/microservice/suenio/posicion/PosicionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ucuenca.microservice.suenio.posicion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosicionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
